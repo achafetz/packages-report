a3e4d2a76c67162faadda87b6e7692774bfc3f96
Aaron <achafetz@usaid.gov>
2018-02-01 11:48:53 -0800

    updates that didn't take

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -2,6 +2,15 @@
 
 ## create a data frame of your installed packages
 
+library(fs)
+library(tidyverse)
+library(magrittr)
+library(here)
+path_real(.Library)
+
+lib <- installed.packages() %>%
+  as_tibble()
+
 ## keep the variables
 ##   * Package
 ##   * LibPath
@@ -9,7 +18,13 @@
 ##   * Priority
 ##   * Built
 
+lib %<>%  select(Package, LibPath, Version, Priority, Built)
+
 ## write it to data/installed-packages.csv
+lib %>%
+write_csv(here("data", "installed-packages.csv"), na = "")
+
+
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
